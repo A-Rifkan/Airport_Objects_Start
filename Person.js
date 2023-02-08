4fa05dccf4f3198603353a2c7d9e32d732565e0b
A-Rifkan <a.rifkan2019@gmail.com>
2023-02-08 11:39:27 +0000

    done

--- Person.js
+++ Person.js
@@ -3,8 +3,15 @@ class Person {
         // 1. Name
         // 2. Bags
             // 2a. Bags should contain/reference a data type for storing more than one bag object.
-
+     constructor (name, bags = []){
+        this.name = name;
+        this.bags = bags;
+    }
+    addBag(newbag){
+            this.bags.push(newbag);
+    }
     // Include an addBag method
 }
 
 //Export the Module
+module.exports = Person;
